3497e6c98d0253423a982543230fd1f2d84ef85e
yangjc <bupt_yjc@163.com>
2019-05-04 16:03:38 +0800

    add augment

--- preprocess.py
+++ preprocess.py
@@ -4,7 +4,7 @@ import re
 
 import pandas as pd
 
-from random import shuffle, sample
+from random import shuffle, sample, randint
 
 from util import load_word_re, load_type_re, load_pair, word_replace
 
@@ -17,6 +17,8 @@ stop_word_re = load_word_re(path_stop_word)
 word_type_re = load_type_re(path_type_dir)
 homo_dict = load_pair(path_homo)
 syno_dict = load_pair(path_syno)
+
+aug_rate, pos_rate, neg_rate = 2, 4, 4
 
 
 def save_triple(path, triples):
@@ -35,30 +37,29 @@ def expand(triples, path_extra_triple):
     return extra_triples + triples
 
 
-def make_triple(path_univ_dir, path_train_triple, path_test_triple, path_extra_triple):
+def make_triple(path_aug_dir, path_train_triple, path_test_triple, path_extra_triple):
     labels = list()
     label_texts = dict()
-    files = os.listdir(path_univ_dir)
+    files = os.listdir(path_aug_dir)
     for file in files:
         label = os.path.splitext(file)[0]
         labels.append(label)
         label_texts[label] = list()
-        with open(os.path.join(path_univ_dir, file), 'r') as f:
+        with open(os.path.join(path_aug_dir, file), 'r') as f:
             for line in f:
                 label_texts[label].append(line.strip())
-    neg_fold = 2
     triples = list()
     for i in range(len(labels)):
         texts = label_texts[labels[i]]
-        neg_texts = list()
+        res_texts = list()
         for j in range(len(labels)):
             if j != i:
-                neg_texts.extend(label_texts[labels[j]])
-        for j in range(len(texts) - 1):
-            for k in range(j + 1, len(texts)):
-                sub_texts = sample(neg_texts, neg_fold)
-                for neg_text in sub_texts:
-                    triples.append((texts[j], texts[k], neg_text))
+                res_texts.extend(label_texts[labels[j]])
+        for text in texts:
+            pos_texts = sample(texts, pos_rate)
+            neg_texts = sample(res_texts, neg_rate)
+            for pos_text, neg_text in zip(pos_texts, neg_texts):
+                triples.append((text, pos_text, neg_text))
     shuffle(triples)
     bound = int(len(triples) * 0.9)
     train_triples = expand(triples[:bound], path_extra_triple)
@@ -74,12 +75,12 @@ def save(path, texts, labels):
             f.write(text + ',' + label + '\n')
 
 
-def gather(path_univ_dir, path_train, path_test):
+def gather(path_aug_dir, path_train, path_test):
     texts, labels = list(), list()
-    files = os.listdir(path_univ_dir)
+    files = os.listdir(path_aug_dir)
     for file in files:
         label = os.path.splitext(file)[0]
-        with open(os.path.join(path_univ_dir, file), 'r') as f:
+        with open(os.path.join(path_aug_dir, file), 'r') as f:
             for line in f:
                 texts.append(line.strip())
                 labels.append(label)
@@ -99,7 +100,28 @@ def clean(text):
     return word_replace(text, syno_dict)
 
 
-def prepare(path_univ_dir):
+def augment(text, name):
+    aug_texts = list()
+    bound = len(text) - 1
+    if bound > 0:
+        for _ in range(aug_rate):
+            words = list(text)
+            if name == 'drop':
+                ind = randint(0, bound)
+                words.pop(ind)
+            elif name == 'swap':
+                ind1, ind2 = randint(0, bound), randint(0, bound)
+                words[ind1], words[ind2] = words[ind2], words[ind1]
+            elif name == 'copy':
+                ind1, ind2 = randint(0, bound), randint(0, bound)
+                words.insert(ind1, words[ind2])
+            else:
+                raise KeyError
+            aug_texts.append(''.join(words))
+    return aug_texts
+
+
+def prepare(path_univ_dir, path_aug_dir):
     files = os.listdir(path_univ_dir)
     for file in files:
         text_set = set()
@@ -111,18 +133,22 @@ def prepare(path_univ_dir):
                 if text and text not in text_set:
                     text_set.add(text)
                     texts.append(text)
-        with open(os.path.join(path_univ_dir, file), 'w') as f:
+                    texts.extend(augment(text, 'drop'))
+                    texts.extend(augment(text, 'swap'))
+                    texts.extend(augment(text, 'copy'))
+        with open(os.path.join(path_aug_dir, file), 'w') as f:
             for text in texts:
                 f.write(text + '\n')
 
 
 if __name__ == '__main__':
     path_univ_dir = 'data/univ'
-    prepare(path_univ_dir)
+    path_aug_dir = 'data/aug'
+    prepare(path_univ_dir, path_aug_dir)
     path_train = 'data/train.csv'
     path_test = 'data/test.csv'
-    gather(path_univ_dir, path_train, path_test)
+    gather(path_aug_dir, path_train, path_test)
     path_train_triple = 'data/train_triple.csv'
     path_test_triple = 'data/test_triple.csv'
     path_extra_triple = 'data/extra_triple.csv'
-    make_triple(path_univ_dir, path_train_triple, path_test_triple, path_extra_triple)
+    make_triple(path_aug_dir, path_train_triple, path_test_triple, path_extra_triple)
